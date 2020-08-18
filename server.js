9ac929efaebc4a5fb3bca0c9d54b5c0024d869c5
Eizeiky777 <ueki.winchester23@gmail.com>
2020-08-18 15:47:08 +0700

    add cors

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 
-app.use(cors());
-
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
+app.use(cors());
+app.options('*', cors());
 
+app.use("/uploads", express.static("uploads"));
 app.use("/api/nofa", router);
 
 app.listen(port, () =>
